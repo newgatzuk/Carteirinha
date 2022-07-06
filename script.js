2578ed3665d7476d42aa596f2cc57e6c797c7fd8
William <74389968+newgatzuk@users.noreply.github.com>
2022-07-06 14:51:14 +0000

    Iniciando Back-end

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-particlesJS.load('particles-container', 'particlesjs-config.json');
+function fundoanimado(){
+    particlesJS.load('particles-container', 'particlesjs-config.json');
+}
+
+fundoanimado();
+const verificarbody = document.querySelector("cadastrocm");
+if(verificarbody){
+    fundoanimado();
+}else{
+    const numero = 0;
+}
+
+
+
+const form = document.getElementById('form');
+const nomecompleto = document.getElementById('nomecompleto');
+const cpfinput = document.getElementById('cpfinput');
+const databatismo = document.getElementById('databatismo');
+const nomedamae = document.getElementById('nomedamae');
+const nomedopai = document.getElementById('nomedopai');
+const foto34 = document.getElementById('foto34');
+
+//form.addEventListener("submit", (e) => {
+//    e.preventDefault();
+//});
